b663116e3bae311a0a2e4f75a06b2977533c8f35
yebin <na04232@naver.com>
2024-05-14 12:11:07 +0900

    Initial Commit

---
+++ Movie_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie_App
+//
+//  Created by 이예빈 on 2024/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
