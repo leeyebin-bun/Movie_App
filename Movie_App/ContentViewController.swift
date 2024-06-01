79c47545cbd44c01d8bd1a729301224eb68d5e2f
yebin <na04232@naver.com>
2024-06-01 15:17:57 +0900

    눈동자 3D 삽입 , start 버튼 이벤트 연결

--- Movie_App/ContentViewController.swift
+++ Movie_App/ContentViewController.swift
@@ -1,44 +1,82 @@
 import SwiftUI
 import SplineRuntime
+import UIKit
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            //3D
-            Header()
-                .frame(height: 500)
+        ZStack{
+            Color.black
+                .edgesIgnoringSafeArea(.all)
             
-            VStack(spacing:30)
-            {
-                Text("Welcome")
-                    .bold()
-                Button("Start")
+            VStack{
+                //3D
+                Header()
+                    .frame(height: 530)
+                
+                VStack(spacing:20)
                 {
+                    Text("Let's eat Movie")
+                        .foregroundStyle(.white)
+                        .fontWeight(.bold)
+                        .font(.system(size: 30))
+                    Text("It will be easier than you think !")
+                        .foregroundColor(Color(UIColor(red: 191/255, green: 255/255, blue: 0/255, alpha: 1.0)))
+                        .font(.system(size: 16))
                     
+                    Button(action: {
+                        Coordinator().presentCreateView()
+                    })
+                    {
+                        Text("Start")
+                            .fontWeight(.bold) // 버튼 텍스트 크기 설정
+                            //.fontWeight(.bold) // 버튼 텍스트 굵게 설정
+                            .padding()
+                            .frame(width: 300)
+                            .background(Color(UIColor(red: 191/255, green: 255/255, blue: 0/255, alpha: 1.0)))
+                            .foregroundColor(.black)
+                            .cornerRadius(20)
+                    }
                 }
+                .padding()
+                Spacer()
             }
-            Spacer()
         }
     }
 }
 
+struct Coordinator {
+    func presentCreateView() {
+        guard let keyWindow = UIApplication.shared.windows.filter({$0.isKeyWindow}).first,
+              let rootViewController = keyWindow.rootViewController else {
+            return
+        }
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let posterVC = storyboard.instantiateViewController(withIdentifier: "PosterView") as? UIViewController else {
+            return
+        }
+        
+        posterVC.modalTransitionStyle = .coverVertical
+        posterVC.modalPresentationStyle = .fullScreen
+        
+        rootViewController.present(posterVC, animated: true, completion: nil)
+    }
+}
 
 struct Header:View {
     var body: some View {
-        let url = URL(string: "https://build.spline.design/JlziB7XG8zYuLskwh5lq/scene.splineswift")!
-        
+        let url = URL(string: "https://build.spline.design/TfTFKrASp3EMpzwRLbgf/scene.splineswift")!
         try? SplineView(sceneFileURL: url).ignoresSafeArea(.all)
     }
 }
 
-struct Movie_App: App { // YourAppNameApp을 실제 프로젝트 이름으로 변경
+struct Movie_App: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
-
 
 /*
 #Preview {
--- Movie_App/MainViewController.swift
+++ Movie_App/MainViewController.swift
@@ -4,41 +4,6 @@ import RealmSwift
 import SwiftUI
 import SplineRuntime
 
-/*
-struct ContentView: View {
-    var body: some View {
-        VStack{
-            //3D
-            Header()
-                .frame(height: 500)
-            
-            VStack(spacing:30)
-            {
-                Text("Welcome")
-                    .bold()
-                Button("Start")
-                {
-                    
-                }
-            }
-            Spacer()
-        }
-    }
-}
-
-struct Header:View {
-    var body: some View {
-        let url = URL(string: "https://build.spline.design/JlziB7XG8zYuLskwh5lq/scene.splineswift")!
-        
-        try? SplineView(sceneFileURL: url).ignoresSafeArea(.all)
-    }
-}
-
-
-#Preview {
-    ContentView()
-}
-*/
 
 class MainViewController: UIViewController {
 
