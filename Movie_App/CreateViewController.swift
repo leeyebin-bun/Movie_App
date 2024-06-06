99be868df2f621d3ce4145f10d3b049b95a33119
yebin <na04232@naver.com>
2024-06-06 16:35:53 +0900

    CreateView 디자인중

--- Movie_App/CreateViewController.swift
+++ Movie_App/CreateViewController.swift
@@ -6,6 +6,9 @@ import SplineRuntime
 struct CreateView: View {
     @State private var isDatePickerVisible = false
     @State private var selectedDate = Date()
+    @State private var title = ""
+    @State private var name = ""
+    @State private var note = ""
     
     var body: some View {
         NavigationView {
@@ -24,27 +27,68 @@ struct CreateView: View {
                     .sheet(isPresented: $isDatePickerVisible) {
                         datePickerView
                     }
+                    
                     Spacer()
                 }
+            
+                VStack(spacing: 100) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 30)
+                            .foregroundColor(Color(UIColor(red: 191/255, green: 255/255, blue: 0/255, alpha: 1.0)))
+                            .frame(width: 60, height: 25)
+                        Text("title")
+                            .foregroundColor(.black)
+                    }
+                    .padding(.leading, 30) // 왼쪽 마진 추가
+
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 30)
+                            .foregroundColor(Color(UIColor(red: 191/255, green: 255/255, blue: 0/255, alpha: 1.0)))
+                            .frame(width: 60, height: 25)
+                        Text("time")
+                            .foregroundColor(.black)
+                    }
+                    .padding(.leading, 30) // 왼쪽 마진 추가
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 30)
+                            .foregroundColor(Color(UIColor(red: 191/255, green: 255/255, blue: 0/255, alpha: 1.0)))
+                            .frame(width: 60, height: 25)
+                        Text("note")
+                            .foregroundColor(.black)
+                    }
+                    .padding(.leading, 30) // 왼쪽 마진 추가
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 30)
+                            .foregroundColor(Color(UIColor(red: 191/255, green: 255/255, blue: 0/255, alpha: 1.0)))
+                            .frame(width: 60, height: 25)
+                        Text("photo")
+                            .foregroundColor(.black)
+                    }
+                    .padding(.leading, 30) // 왼쪽 마진 추가
+                }
+                .padding(.bottom , 150)
+                .frame(maxWidth: .infinity, alignment: .leading) // 내부 요소의 정렬을 왼쪽으로
             }
+            //.frame(maxWidth: .infinity, alignment: .leading) // 최대 너비 지정
         }
     }
     
+    
     private var datePickerView: some View {
         DatePicker("", selection: $selectedDate, displayedComponents: .date)
-                    .datePickerStyle(GraphicalDatePickerStyle())
-                    .padding()
-                    .onChange(of: selectedDate) { _ in
-                        isDatePickerVisible = false
-                    }
-       }
+            .datePickerStyle(GraphicalDatePickerStyle())
+            .padding()
+            .onChange(of: selectedDate) { _ in
+                isDatePickerVisible = false
+            }
+    }
     
     private let dateFormatter: DateFormatter = {
-          let formatter = DateFormatter()
-            formatter.dateStyle = .medium
-            return formatter
-            }()
-
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter
+    }()
+    
 }
 
 #Preview {
