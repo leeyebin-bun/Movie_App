fb546e7f3b7310223dcf717454cad87fad920568
yebin <na04232@naver.com>
2024-06-06 14:40:10 +0900

    DatePicker 추가중

--- Movie_App/CreateViewController.swift
+++ Movie_App/CreateViewController.swift
@@ -1,64 +1,52 @@
-//
-//  CreateViewController.swift
-//  Movie_App
-//
-//  Created by 이예빈 on 2024/05/22.
-//
-
 import UIKit
+import RealmSwift
+import SwiftUI
+import SplineRuntime
 
-class CreateViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+struct CreateView: View {
+    @State private var isDatePickerVisible = false
+    @State private var selectedDate = Date()
     
-    @IBOutlet weak var doneButton: UIButton!
-    @IBOutlet weak var itemImg: UIImageView!
-    @IBOutlet weak var backButton: UIButton!
-    @IBOutlet weak var navigationBar: UINavigationBar!
-    @IBOutlet weak var titleTextField: UITextField!
-    
-    let imgPickerController = UIImagePickerController()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.doneButton.layer.masksToBounds = true
-        self.doneButton.layer.cornerRadius = 6
-        self.doneButton.titleLabel?.font = UIFont.systemFont(ofSize: 13)
-        
-        self.navigationBar.layer.masksToBounds = true
-        self.navigationBar.layer.cornerRadius = 15
-        
-        imgPickerController.delegate = self
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.black
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Button(action: {
+                        isDatePickerVisible = true
+                    }) {
+                        Text(dateFormatter.string(from: selectedDate))
+                            .padding()
+                            .foregroundColor(.white)
+                    }
+                    .sheet(isPresented: $isDatePickerVisible) {
+                        datePickerView
+                    }
+                    Spacer()
+                }
+            }
+        }
     }
     
-    /*
-     뒤로가기
-     */
-    @IBAction func OnClickBackButton(_ sender: Any) {
-        self.presentingViewController?.dismiss(animated: true)
-    }
+    private var datePickerView: some View {
+        DatePicker("", selection: $selectedDate, displayedComponents: .date)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                    .padding()
+                    .onChange(of: selectedDate) { _ in
+                        isDatePickerVisible = false
+                    }
+       }
     
-    /*
-     생성완료
-     */
-    @IBAction func OnClickDoneButton(_ sender: Any) {
-        
-        guard let vc = self.storyboard?.instantiateViewController(identifier: "PosterViewController") as? PosterViewController else { return }
-        
-        //vc.dataDelegate = self
-        self.navigationController?.pushViewController(vc, animated: true)
-        
-        // PosterView 로 이동
-        guard let posterVC = self.storyboard?.instantiateViewController(identifier: "PosterView") else { return }
-        posterVC.modalTransitionStyle = .coverVertical
-        posterVC.modalPresentationStyle = .fullScreen
-        
-        self.present(posterVC, animated: true, completion: nil)
-    }
-    
-    @IBAction func addImgAction(_ sender: Any) {
-        // 이미지 소스로 사진 라이브러리 선택
-        imgPickerController.sourceType = .photoLibrary
-        // 이미지 피커 컨트롤러 실행
-        self.present(imgPickerController, animated: true, completion: nil)
-    }
+    private let dateFormatter: DateFormatter = {
+          let formatter = DateFormatter()
+            formatter.dateStyle = .medium
+            return formatter
+            }()
+
+}
+
+#Preview {
+    CreateView()
 }
--- Movie_App/MainViewController.swift
+++ Movie_App/MainViewController.swift
@@ -6,13 +6,14 @@ import SplineRuntime
 
 
 struct MainView: View {
+    @State private var isCreateViewPresented = false
     var body: some View {
         ZStack{
             Color.black
                 .edgesIgnoringSafeArea(.all)
             
             VStack{
-                MenuBar()
+                MenuBar(isCreateViewPresented: $isCreateViewPresented)
                 .padding()
                 Spacer()
             }
@@ -21,13 +22,15 @@ struct MainView: View {
 }
 
 struct MenuBar: View {
+    @Binding var isCreateViewPresented: Bool
+    // MenuBar 의 상태를 MainView 에 전달하고 업데이트
     var body: some View {
         ZStack {
             // 배경색 설정
             Color(red: 191/255, green: 255/255, blue: 0/255)
                 .frame(width: 330 , height: 45)
                 .cornerRadius(13)
-                
+            
             HStack {
                 Button(action: {
                     // 버튼 액션
@@ -42,60 +45,21 @@ struct MenuBar: View {
                 Spacer()
                 
                 Button(action: {
-                    // 버튼 액션
-                    
+                    self.isCreateViewPresented.toggle()
                 }) {
-                    Text("...")
+                    Image(systemName: "plus")
                         .foregroundColor(.black)
                         .font(.system(size: 15))
-                        .bold()
                         .padding(.trailing, 40)
                 }
             }
             .padding(.vertical, 20)
         }
-    }
-}
-
-/*
-class MainViewController: UIViewController {
-
-    @IBOutlet weak var startButton: UIButton!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-       
-        //버튼 폰트 크기,굵기,넓이
-        self.startButton.titleLabel?.font = UIFont.systemFont(ofSize: 13, weight: .bold, width: .standard)
-        
-        //저장할 데이터 생성
-        let user = Model()
-        user.name = "yebong"
-        user.age = 28
-        
-        //Realm 불러오기
-        let realm = try! Realm()
-        //Realm 파일 위치
-        print(Realm.Configuration.defaultConfiguration.fileURL!)
-        
-        //데이터 저장하기
-        try! realm.write{
-            realm.add(user)
+        .sheet(isPresented: $isCreateViewPresented) {
+            CreateView()
         }
-        
-        //print("users", realm.objects(Model.self))
-        
-        //데이터 가져오기
-        let myUser = realm.objects(Model.self).filter("name == 'yebong'").first
-        //print("myUsers",myUser!)
-        
-        //IOS 다크모드 / 라이트모드 적용
-        //self.view.backgroundColor = .systemBackground
-       
     }
-
 }
-*/
 
 #Preview {
     MainView()
