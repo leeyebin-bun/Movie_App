d0071d733e830983338983fef1837b656213d4ae
yebin <na04232@naver.com>
2024-06-11 23:09:21 +0900

    사진 데이터 추가중

--- Movie_App/CreateViewController.swift
+++ Movie_App/CreateViewController.swift
@@ -6,6 +6,7 @@ import SwiftUI
 class MyDataModel: Object , Identifiable {
     @Persisted var titleText: String = ""
     @Persisted var timeText: String = ""
+    @Persisted var imageData: Data?
 }
 
 struct CreateView: View {
