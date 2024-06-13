12ed56218c6218611735d67dab9c5fdf71f40de9
yebin <na04232@naver.com>
2024-06-13 18:22:46 +0900

    MVVM 모델로 변경

---
+++ Movie_App/Presentation/ViewController/MainView.swift
@@ -0,0 +1,12 @@
+//
+//  MainView.swift
+//  Movie_App
+//
+//  Created by 이예빈 on 6/13/24.
+//
+
+import UIKit
+
+class MainView: NSObject {
+
+}
---
+++ Movie_App/Presentation/ViewModel/MovieDataModel.swift
@@ -0,0 +1,12 @@
+//
+//  MovieDataModel.swift
+//  Movie_App
+//
+//  Created by 이예빈 on 6/13/24.
+//
+
+import UIKit
+
+class MovieDataModel: NSObject {
+
+}
