ced14b8dbc029381322dd00e12c4e7003e462b15
yebin <na04232@naver.com>
2024-07-14 23:42:01 +0900

    CreateBGView 임시 삭제

--- Movie_App/Presentation/ViewController/CreateBGViewController.swift
+++ Movie_App/Presentation/ViewController/CreateBGViewController.swift
@@ -1,50 +1,50 @@
-import SwiftUI
-import RealmSwift
-
-struct CreateBGView : View {
-    @ObservedObject var viewModel = MyDataViewModel()
-    @State private var isPresented = false
-    
-    var body: some View {
-        NavigationView {
-            ZStack {
-                Color.black
-                    .edgesIgnoringSafeArea(.all)
-                VStack {
-                    Text("행성을 선택해주세요")
-                        .foregroundColor(Color.white)
-                        .font(.title3)
-                    
-                    HStack(spacing:20){
-                        Button(action: {
-                            isPresented = true
-                        }) {
-                            Circle()
-                                .fill(Color.blue)
-                                .frame(width: 100, height: 100)
-                            
-                        }
-                        .sheet(isPresented: $isPresented) {
-                            MainImageView(viewModel: viewModel)
-                        }
-                        Button(action: {
-                        }) {
-                            
-                            Circle()
-                                .fill(Color.green)
-                                .frame(width: 100, height: 100)
-                        }
-                        
-                    }
-                    .padding(40)
-                }
-                
-            }
-        }
-        
-    }
-}
-
-#Preview {
-    CreateBGView()
-}
+//import SwiftUI
+//import RealmSwift
+//
+//struct CreateBGView : View {
+//    @ObservedObject var viewModel = MyDataViewModel()
+//    @State private var isPresented = false
+//    
+//    var body: some View {
+//        NavigationView {
+//            ZStack {
+//                Color.black
+//                    .edgesIgnoringSafeArea(.all)
+//                VStack {
+//                    Text("행성을 선택해주세요")
+//                        .foregroundColor(Color.white)
+//                        .font(.title3)
+//                    
+//                    HStack(spacing:20){
+//                        Button(action: {
+//                            isPresented = true
+//                        }) {
+//                            Circle()
+//                                .fill(Color.blue)
+//                                .frame(width: 100, height: 100)
+//                            
+//                        }
+//                        .sheet(isPresented: $isPresented) {
+//                            MainImageView(viewModel: viewModel)
+//                        }
+//                        Button(action: {
+//                        }) {
+//                            
+//                            Circle()
+//                                .fill(Color.green)
+//                                .frame(width: 100, height: 100)
+//                        }
+//                        
+//                    }
+//                    .padding(40)
+//                }
+//                
+//            }
+//        }
+//        
+//    }
+//}
+//
+//#Preview {
+//    CreateBGView()
+//}
